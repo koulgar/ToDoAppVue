c55a9b2ca221128066ee05f5f4b8b052d0983f56
Doğucan Durmuşlar <ddurmuslar@hurriyetemlak.com>
2020-08-09 17:42:11 +0300

    first blood

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <div id="header-top" class="header-sub-cage">
+      <div id="header-top-title" class="header-sub-cage__middle col-4">
+        <span>To Do App</span>
+      </div>
+      <div id="user-content" class="header-sub-cage__right col-4">
+        <img id="user-logo" src="../assets/logo.png" alt="">
+        <span id="user-info">Koulgar</span>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: 'Header'
+}
+
+</script>
