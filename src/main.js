205435bb08fb1678411f39f8420fbd93c03ab663
Doğucan Durmuşlar <ddurmuslar@hurriyetemlak.com>
2020-08-09 17:39:53 +0300

    first blood

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import "./assets/main.scss";
+
+require('bootstrap');
 
 Vue.config.productionTip = false
 
